d476f5edaf90153737188ecc510ac69f5d4b9075
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-22 21:19:33 +0300

    hello world :)

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello, World!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
+}
